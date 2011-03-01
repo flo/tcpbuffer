abf2dc6a462302501270dd38659cbed5802d975c
Florian Köberle <florian@fkoeberle.de>
2011-03-01 11:12:10 +0100

    Removed compile warning from generated code.

--- src/de/fkoeberle/tcpbuffer/ApplicationFrame.java
+++ src/de/fkoeberle/tcpbuffer/ApplicationFrame.java
@@ -428,10 +428,7 @@ public class ApplicationFrame extends JFrame {
 
 	private static void installLnF() {
 		try {
-			String lnfClassname = PREFERRED_LOOK_AND_FEEL;
-			if (lnfClassname == null)
-				lnfClassname = UIManager.getCrossPlatformLookAndFeelClassName();
-			UIManager.setLookAndFeel(lnfClassname);
+			UIManager.setLookAndFeel(PREFERRED_LOOK_AND_FEEL);
 		} catch (Exception e) {
 			System.err.println("Cannot install " + PREFERRED_LOOK_AND_FEEL
 					+ " on this platform:" + e.getMessage());
