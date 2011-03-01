338ef4b479d27d58c6c9814007bcf7f16a900d43
Florian Köberle <florian@fkoeberle.de>
2011-03-01 11:09:07 +0100

    Put the source under CC0.

--- src/de/fkoeberle/tcpbuffer/HostingListener.java
+++ src/de/fkoeberle/tcpbuffer/HostingListener.java
@@ -1,3 +1,14 @@
+/*
+ * This file is made available under the Creative Commons CC0 1.0 Universal Public Domain Dedication:
+ * 
+ * http://creativecommons.org/publicdomain/zero/1.0/deed.en
+ * 
+ * The person who associated a work with this deed has dedicated the work to the public domain 
+ * by waiving all of his or her rights to the work worldwide under copyright law, including all
+ * related and neighboring rights, to the extent allowed by law. You can copy, modify, distribute 
+ * and perform the work, even for commercial purposes, all without asking permission. 
+ * 
+ */
 package de.fkoeberle.tcpbuffer;
 
 public interface HostingListener {
